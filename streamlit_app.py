9f89a737f6361ed3db9a326f5213ac52bd698191
Agos95 <agostini.federico.95@gmail.com>
2025-10-18 22:13:20 +0200

    add some cache

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import streamlit as st
 
 import pdf_parsing
@@ -8,16 +9,38 @@ st.set_page_config(
 st.logo("assets/icon.svg", link="https://www.pallacanestroarcella.com/")
 
 _ST_TEAM_MAPPING_KEY = "team_rename"
+_DEFAULT_TEAM = "arcella"
 
 parse_pdf = st.cache_data(pdf_parsing.parse_pdf)
 
 
+@st.cache_data
+def _get_default_team(teams: list[str]) -> int:
+    for i, team in enumerate(teams):
+        if _DEFAULT_TEAM in team.lower():
+            break
+    return i
+
+
+@st.cache_data
+def _filter_df(df: pd.DataFrame, team: str) -> pd.DataFrame:
+    df = df.loc[(df["Home"] == team) | (df["Away"] == team)].reset_index(drop=True)
+    df = df.set_index([[i + 1 for i in range(len(df))]])
+    return df
+
+
 @st.cache_data()
-def _replace_team(x: str, mapping: dict[str, str | None]) -> str:
-    new_name = mapping.get(x, None)
-    if new_name:
-        return new_name
-    return x
+def _replace_team(df: pd.DataFrame, mapping: dict[str, str | None]) -> str:
+    def _rename(x):
+        new_name = mapping.get(x, None)
+        if new_name:
+            return new_name
+        return x
+
+    df["Home"] = df["Home"].apply(_rename)
+    df["Away"] = df["Away"].apply(_rename)
+
+    return df
 
 
 st.title("Arcella Basket Utilities")
@@ -26,42 +49,37 @@ team_selector_col, file_upload_col = st.columns([0.5, 0.5])
 
 with file_upload_col:
     file = st.file_uploader("Upload PDF", type="pdf")
+
 if file:
     df = parse_pdf(file)
     teams = sorted(df["Home"].unique())
     if _ST_TEAM_MAPPING_KEY not in st.session_state:
         st.session_state[_ST_TEAM_MAPPING_KEY] = {t: "" for t in teams}
 
-    index = None
-    for i, team in enumerate(teams):
-        if "arcella" in team.lower():
-            index = i
-            break
-
     with team_selector_col:
         team = st.selectbox(
-            "Filter Teams", options=teams, index=index, key="team_filter"
+            "Filter Teams",
+            options=teams,
+            index=_get_default_team(teams),
         )
 
     table_col, rename_col = st.columns([0.7, 0.3], gap="large")
 
     with table_col:
         if team:
-            df = df.loc[(df["Home"] == team) | (df["Away"] == team)].reset_index(
-                drop=True
-            )
-            df = df.set_index([[i + 1 for i in range(len(df))]])
-
+            df = _filter_df(df, team)
         rename = st.session_state.get(_ST_TEAM_MAPPING_KEY, None)
         if rename is not None:
-            df["Home"] = df["Home"].apply(_replace_team, mapping=rename)
-            df["Away"] = df["Away"].apply(_replace_team, mapping=rename)
+            df = _replace_team(df, mapping=rename)
 
         st.markdown("#### Games")
         st.dataframe(df, hide_index=False)
 
         st.download_button(
-            "Download", data=df.to_csv(index=None), file_name="calendar.csv"
+            "Download",
+            data=df.to_csv(index=None),
+            file_name="calendar.csv",
+            icon=":material/download:",
         )
 
     with rename_col:
